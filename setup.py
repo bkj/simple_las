dfe50cdac74070b5dbb8c10a153455048c87bf83
bjohnson <ben@gophronesis.com>
2017-08-01 17:26:18 -0400

    turning into a package

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env/python
+
+from setuptools import setup
+
+setup(
+    name='simple_las',
+    author='Ben Johnson',
+    author_email='bkj.322@gmail.com',
+    classifiers=[],
+    description='simple linearized active search',
+    keywords=['simple_las'],
+    license='MIT',
+    packages=['simple_las'],
+    version="0.0.0"
+)
